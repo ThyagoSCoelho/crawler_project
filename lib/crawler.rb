cc1282bfd1f27a7bbd30214b06b9261271eb50bd
Coelho <thystockler@gmail.com>
2023-06-18 17:56:37 -0300

    fix(crawler): fixed warnings rubocop

--- lib/crawler.rb
+++ lib/crawler.rb
@@ -1,28 +1,29 @@
+# frozen_string_literal: true
+
 require_relative '../helper/url_builder'
 
 require 'mechanize'
 require 'json'
 
 class Crawler
-	def initialize
-		@agent = Mechanize.new
-	end
+  def initialize
+    @agent = Mechanize.new
+  end
 
-	def fetch_json(url, options = {})
-		response = @agent.get(build_url(url, options))
-		json = JSON.parse(response.body)
-		return json
-	end
+  def fetch_json(url, options = {})
+    response = @agent.get(build_url(url, options))
+    JSON.parse(response.body)
+  end
 
-	def fetch_image(url, route)
-		response = @agent.get(url + route)
-		return response.body
-	end
+  def fetch_image(url, route)
+    response = @agent.get(url + route)
+    response.body
+  end
 
-	private
+  private
 
-	def build_url(url, options)
-		url_builder = URLBuilder.new(url, options)
-		url_builder.build_url
-	end
+  def build_url(url, options)
+    url_builder = URLBuilder.new(url, options)
+    url_builder.build_url
+  end
 end
