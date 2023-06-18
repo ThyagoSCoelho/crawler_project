69bf2c71f166ebea1fd0f064695ed442ca925a8b
Coelho <thystockler@gmail.com>
2023-06-18 16:07:56 -0300

    chore: added crawler generic lib

---
+++ lib/crawler.rb
@@ -0,0 +1,29 @@
+require_relative '../helper/url_builder'
+
+require 'mechanize'
+require 'json'
+
+class Crawler
+	def initialize
+		@agent = Mechanize.new
+	end
+
+	def fetch_json(url, options = {})
+		binding.pry
+		response = @agent.get(build_url(url, options))
+		json = JSON.parse(response.body)
+		return json
+	end
+
+	def fetch_image(url, route)
+		response = @agent.get(url + route)
+		return response.body
+	end
+
+	private
+
+	def build_url(url, options)
+		url_builder = URLBuilder.new(url, options)
+		url_builder.build_url
+	end
+end
