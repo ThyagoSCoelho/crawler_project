a46a24be37cfbd490bf7845b4b457d2656111098
Coelho <thystockler@gmail.com>
2023-06-19 02:09:34 -0300

    chore(my_new_car_crawler): changed path

---
+++ lib/my_new_car_crawler.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require_relative 'services/crawler'
+require_relative 'logger'
+require 'pry'
+
+MEU_CARRO_NOVO_API = 'https://www.meucarronovo.com.br/api/v2/busca'
+
+class MyNewCarCrawler
+  def initialize
+    @crawler = Crawler.new
+  end
+
+  def call(options)
+    @options_search = options
+    search
+  end
+
+  private
+
+  def search
+    @json_data = @crawler.fetch_json(MEU_CARRO_NOVO_API, @options_search)
+    documents = @json_data['documentos']
+
+    return 'No results found' if documents.empty?
+
+    save(documents, @options_search)
+    pagination
+  end
+
+  def pagination
+    {
+      current_page: @json_data['paginaCorrente'],
+      final_page: @json_data['ultimaPagina']
+    }
+  end
+
+  def save(documents, options)
+    logger = Logger.new(documents, options)
+    logger.save
+  end
+end
